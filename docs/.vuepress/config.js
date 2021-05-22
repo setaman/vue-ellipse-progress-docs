e82190b8739b2a73c54decc544df5e9a9e606375
Sergej Atamantschuk <sergejatam@gmail.com>
2021-05-22 13:09:02 +0200

    feat: init vuepress project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    lang: 'en-US',
+    title: 'vue-ellipse-progress documentation',
+    description: 'This is my first VuePress site',
+
+    themeConfig: {
+        logo: 'https://vuejs.org/images/logo.png',
+    },
+    base: "/vue-ellipse-progress-docs/"
+}
