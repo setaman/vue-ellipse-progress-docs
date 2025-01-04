e1d961b19bc507fe60b0376414496e94765a5524
Sergej Atamantschuk <sergejatam@gmail.com>
2025-01-04 16:18:44 +0100

    fix: line snippet
    Signed-off-by: Sergej Atamantschuk <sergejatam@gmail.com>

--- .vitepress/theme/Guide/Line/Snippet1.vue
+++ .vitepress/theme/Guide/Line/Snippet1.vue
@@ -1,5 +1,6 @@
 <template>
   <ve-progress line="round" :thickness="15" :progress="50" />
-  <ve-progress line="butt" :thickness="15" :progress="50" />
   <ve-progress line="square" :thickness="15" :progress="50" />
+  <ve-progress line="butt" :thickness="15" :progress="50" />
 </template>
+<script setup lang="ts"></script>
